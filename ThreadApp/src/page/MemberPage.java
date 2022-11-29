b68673173d2531b63e827b3eb8dd8ef941ef7a12
해영 <godud425@naver.com>
2022-11-29 13:51:25 +0900

    깃 이동

---
+++ ThreadApp/src/page/MemberPage.java
@@ -0,0 +1,15 @@
+package page;
+
+import java.awt.Color;
+import java.awt.Dimension;
+
+import javax.swing.JPanel;
+
+//회원가입의 내용을 보여줄 페이지
+public class MemberPage extends JPanel {
+	public MemberPage() {
+	
+		setBackground(Color.pink);
+		setPreferredSize(new Dimension(AppMain.PAGE_WIDTH, AppMain.PAGE_HEIGHT));
+	}
+}
